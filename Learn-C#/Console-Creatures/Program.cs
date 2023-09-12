448ac2dd25fcebaa72c534f95b862fbfd8391890
Evan <eroberts1998@icloud.com>
2023-09-12 14:17:40 -0400

    console creatures added

---
+++ Learn-C#/Console-Creatures/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // prints creature ascii art to terminal
+      Console.WriteLine(" .-.");
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");    
+      Console.WriteLine("|   |");   
+      Console.WriteLine("'~~~'"); 
+      Console.WriteLine("This is Franklin");  
+     }
+  }
+}
